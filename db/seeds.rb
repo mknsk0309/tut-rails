a401f9a0223c4c8d67306daae26a788fa0ca62fa
mknsk0309 <156224339+mknsk0309@users.noreply.github.com>
2025-01-15 02:11:52 +0000

    add : #7 ユーザテーブルのカラム追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,10 @@
 # ユーザレコードの作成
 5.times do |n|
-  User.create!(
-    email_address: "user#{n+1}@example.com",
+  user = User.create!(
+    account_id: "account#{n + 1}",
+    name: "ユーザ#{n + 1}",
+    email_address: "user#{n + 1}@example.com",
     password: "password"
   )
+  user.update!(birthday: Date.today.strftime("%Y/%m/%d")) if n > 0
 end
